d7211c12bd80e9dc2016cee3fc1bd8107de5facc
Irtaza Chohan <irtazachohan@gmail.com>
2019-11-28 22:56:25 +0000

    Updated code for installation to occur in one function as HP SA support IP and non-IP address in their installation path. Also updated help section and other random edits.

--- HP_SA_Install.ps1
+++ HP_SA_Install.ps1
@@ -1,25 +1,76 @@
-﻿# #############################################################################
-# NAME: HP_SA_Install.ps1
-# 
-# AUTHOR:  Irtaza Chohan
-# DATE:  18/06/2015
-# 
-# COMMENT:  This script will install HP SA - if an IP address is entered then it will process that; ensure that the IP entered is a HP SA server which is contactable otherwise the install of HP SA will fail.
-#           If there is no IP entered then the install will install the dormant version of HP SA.
-#
-# VERSION HISTORY
-# 1.0 18/06/2015 Initial Version.
-# 2.0 01/02/2017 Updated to cater for Windows Server 2012 R2 binaries
-#
-# #############################################################################
+﻿<#
+
+.SYNOPSIS
+
+This script installs HP SA.
+
+Written by Irtaza Chohan (http://www.lostintheclouds.net & https://github.com/IrtazaChohan/Align-Crypto-Policy)
+
+.DESCRIPTION
+
+Simply put - installs HP SA on a server. You need to supply an IP address of the HP SA control server (that should be setup before this is installed on clients)
+
+This has been tested on:
+
+- Windows Server 2008
+- Windows Server 2008 R2
+- Windows Server 2012 R2
+- Windows Server 2016
+- Windows Server 2019
+
+NOTES:
+
+ 1. You need to have Administrative rights on the server to run this script. 
+ 2. Please provide a logfile path so log path is saved accordingly.
+ 3. The logPath will also contain the log folder of HP SA.
+ 4. Ensure that you specify a HP SA server (IP Address) as part of this installation.
+ 
+.PARAMETER LogFilePath
+
+Mandatory Parameter
+
+Please specify where on the server you want to keep the logfile that is generated.
+
+.PARAMETER SA_Server1
+
+Optional Parameter
+
+Please specify the HP SA control server (IP address). Its best to have this installed prior to client deployment of HP SA. 
+
+If you do not supply a HP SA server then the installation will install a dorment version of the client; thus you 
+will need to revisit the client to ensure that the HP SA server is specified in the config file.
+
+
+.EXAMPLE
+
+This will install the live version of HP SA client (specifying the IP address of the HP SA server).
+
+.\HP_SA_Install.ps1 -LogFilePath c:\temp -SA_Server1 <IP>
+
+
+.NOTES
+
+ 1. You need to have Administrative rights on the server to run this script. 
+ 2. Please provide a logfile path so log path is saved accordingly.
+ 3. The logPath will also contain the log folder of HP SA.
+
+.LINK
+
+http://www.lostintheclouds.net & https://github.com/IrtazaChohan/Align-Crypto-Policy
+
+#>
+
 
 Param(
-  [string]$SA_Server1
+    [Parameter(Mandatory=$True,
+    HelpMessage="Please view the help for this script; type in ""get-help <scriptname>.ps1""")]
+    [string]$LogFilePath,
+    [string]$SA_Server1
 )
 
 function writelog([string]$result, [string]$logfile) {
     try {
-        $objlogfile = new-object system.io.streamwriter("c:\buildlog\complogs\$logfile", [System.IO.FileMode]::Append)
+        $objlogfile = new-object system.io.streamwriter("$LogFilePath\$logfile", [System.IO.FileMode]::Append)
         $objlogfile.writeline("$((Get-Date).ToString()) : $result")
         write-host (Get-Date).ToString() " : $result"  -foregroundcolor yellow
         $objlogfile.close()
@@ -29,7 +80,7 @@ function writelog([string]$result, [string]$logfile) {
    }
 } 
 
-$log = "HP_SA_Install_x64_RU2017.log"
+$log = "HP_SA_Install_x64.log"
 $ScriptName = $MyInvocation.MyCommand.Name
 
     writelog "================================" $log
@@ -41,13 +92,13 @@ $ScriptName = $MyInvocation.MyCommand.Name
 $ErrorActionPreference = "SilentlyContinue"
 
 #Create folder to store HP SA Log file
-New-Item C:\buildlog\HP_SA -type directory -ErrorAction SilentlyContinue
+New-Item $LogFilePath\HP_SA -type directory -ErrorAction SilentlyContinue
 
-$loggingfile = Get-item C:\buildlog\HP_SA\*.log -ErrorAction SilentlyContinue
+$loggingfile = Get-item $LogFilePath\HP_SA\*.log -ErrorAction SilentlyContinue
 
 If (Test-Path $loggingfile -ErrorAction Ignore)
 {
-	writelog "Removing older HP_SA log file" $log
+	writelog "Removing any older HP_SA log files" $log
     Remove-Item $loggingfile
 }
 else {
@@ -65,8 +116,8 @@ function checkIP([string]$IP){
         [array]$Serv1 = $IP.split(‘.’)
     
            if ($Serv1.length -ne 4) {
-                writelog "ERROR: The IP entered is not valid. IP entered was: $SA_Server1” $log
-                throw “The IP entered is not valid. IP entered was: $SA_Server1” 
+                writelog "ERROR: The IP entered is not valid. IP entered was: $SA_Server1" $log
+                throw "The IP entered is not valid. IP entered was: $SA_Server1"
                 $Correct = $False                          
             }
          }
@@ -122,181 +173,93 @@ function portFree(){
 }
 
 function OSandInstallIP([string]$IP){
-    
-    Try{
-     
-        $ScriptPath = $PSScriptRoot
-        #Let's determine the OS version to install
-        $OSver = (Get-CimInstance Win32_OperatingSystem).version
-        $OSShort = $OSver.Substring(0,3)
-        #check if it is 32-bit or 64-bit
-        $architectureVersion = (Get-CimInstance Win32_Processor -Filter "DeviceID='CPU0'").AddressWidth
+        Try{
         
-        $OSShort | ForEach-Object{
+            $ScriptPath = $PSScriptRoot
+            #Let's determine the OS version to install
+            $OSver = (Get-CimInstance Win32_OperatingSystem).version
+            $OSShort = $OSver.Substring(0,3)
+            #check if it is 32-bit or 64-bit
+            $architectureVersion = (Get-CimInstance Win32_Processor -Filter "DeviceID='CPU0'").AddressWidth
+            
+            $OSShort | ForEach-Object{
 
-        switch -Wildcard ($_)
-        {
-             "6.3"{
-                    writelog "This is Windows Server 2012 R2 - Version found is: $($_)" $log
-                    If($architectureVersion -eq "64"){
-                    writelog "64-bit architecture found" $log
-                    Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.3-X64.exe" -ArgumentList "-s","--logfile","C:\buildlog\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
-                    checkfile
+            switch -Wildcard ($_)
+            {
+                "6.3"{
+                        writelog "This is Windows Server 2012 R2 - Version found is: $($_)" $log
+                        writelog "64-bit architecture found" $log
+                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.3-X64.exe" -ArgumentList "-s","--logfile","$LogFilePath\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
+                        checkfile
+                    }
+                "6.1"{
+                        writelog "This is Windows Server 2008 R2 SP1 - Version found is: $($_)" $log
+                        writelog "64-bit architecture found" $log
+                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.1-X64.exe" -ArgumentList "-s","--logfile","$LogFilePath\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
+                        checkfile
+                    }
+                "6.0"{
+                        writelog "This is Windows Server 2008 SP2 - Version found is: $($_)" $log
+                        If($architectureVersion -eq "64"){
+                            writelog "64-bit architecture found" $log
+                            Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.0-X64.exe" -ArgumentList "-s","--logfile","$LogFilePath\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
+                            checkfile
+                        }
+                        else{
+                            writelog "32-bit architecture found" $log
+                            Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.0.exe" -ArgumentList "-s","--logfile","$LogFilePath\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
+                            checkfile
+                        }
+                    }
+                "5.2"{
+                    Write-Host "This is Windows Server 2003 - Version found is: $($_)"
+                        If($architectureVersion -eq "64"){
+                            writelog "64-bit architecture found" $log
+                            Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-5.2-X64.exe" -ArgumentList "-s","--logfile","$LogFilePath\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
+                            checkfile
+                        }
+                        else{
+                            writelog "32-bit architecture found" $log
+                            Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-5.2.exe" -ArgumentList "-s","--logfile","$LogFilePath\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
+                            checkfile
+                        }
                     }
             }
-            "6.1"{
-                    writelog "This is Windows Server 2008 R2 SP1 - Version found is: $($_)" $log
-                    If($architectureVersion -eq "64"){
-                    writelog "64-bit architecture found" $log
-                    Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.1-X64.exe" -ArgumentList "-s","--logfile","C:\buildlog\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
-                    checkfile
-                    }
-                  }
-            "6.0"{
-                    writelog "This is Windows Server 2008 SP2 - Version found is: $($_)" $log
-                    If($architectureVersion -eq "64"){
-                        writelog "64-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.0-X64.exe" -ArgumentList "-s","--logfile","C:\buildlog\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
-                        checkfile
-                    }
-                    else{
-                        writelog "32-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.0.exe" -ArgumentList "-s","--logfile","C:\buildlog\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
-                        checkfile
-                    }
-                  }
-            "5.2"{
-                   Write-Host "This is Windows Server 2003 - Version found is: $($_)"
-                    If($architectureVersion -eq "64"){
-                        writelog "64-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-5.2-X64.exe" -ArgumentList "-s","--logfile","C:\buildlog\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
-                        checkfile
-                    }
-                    else{
-                        writelog "32-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-5.2.exe" -ArgumentList "-s","--logfile","C:\buildlog\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
-                        checkfile
-                    }
-            }
-         }
         }
-      }
+    }
      Catch{
-
-     writelog "Error occurred in installation of agent" $log
-     $Error[0]
-     Exit -1
-     } 
-}
-
-function OSandInstallDormant(){
-
-    Try{
-     
-        $ScriptPath = $PSScriptRoot
-        #Let's determine the OS version to install
-        $OSver = (Get-CimInstance Win32_OperatingSystem).version
-        $OSShort = $OSver.Substring(0,3)
-        #check if it is 32-bit or 64-bit
-        $architectureVersion = (Get-CimInstance Win32_Processor -Filter "DeviceID='CPU0'").AddressWidth
-        
-        $OSShort | ForEach-Object{
-
-        switch -Wildcard ($_)
-        {
-             "6.3"{
-                    writelog "This is Windows Server 2012 R2 - Version found is: $($_)" $log
-                    If($architectureVersion -eq "64"){
-                    writelog "64-bit architecture found" $log
-                    Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.3-X64.exe" -ArgumentList "-s","--logfile","C:\buildlog\HP_SA\HP_SA_install.log","--loglevel", "info","--withmsi","--opsw_gw_addr_list","$IP","--force_new_device" -Wait
-                    checkfile
-                    }
-            }
-            "6.1"{
-                    writelog "This is Windows Server 2008 R2 SP1 - Version found is: $($_)" $log
-                    If($architectureVersion -eq "64"){
-                    writelog "64-bit architecture found" $log
-                    Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.1-X64.exe" -ArgumentList "-f", "-s", "--withmsi", "--no_opsw_gw", "--logfile", "C:\buildlog\HP_SA\HP_SA_install.log", "--loglevel", "info" -Wait
-                    checkfile
-                    }
-                  }
-            "6.0"{
-                    writelog "This is Windows Server 2008 SP2 - Version found is: $($_)" $log
-                    If($architectureVersion -eq "64"){
-                        writelog "64-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.0-X64.exe" -ArgumentList "-f", "-s", "--withmsi", "--no_opsw_gw", "--logfile", "C:\buildlog\HP_SA\HP_SA_install.log", "--loglevel", "info" -Wait
-                        checkfile
-                    }
-                    else{
-                        writelog "32-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-6.0.exe" -ArgumentList "-f", "-s", "--withmsi", "--no_opsw_gw", "--logfile", "C:\buildlog\HP_SA\HP_SA_install.log", "--loglevel", "info" -Wait
-                        checkfile
-                    }
-                  }
-            "5.2"{
-                   writelog "This is Windows Server 2003 - Version found is: $($_)" $log
-                    If($architectureVersion -eq "64"){
-                        writelog "64-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-5.2-X64.exe" -ArgumentList "-f", "-s", "--withmsi", "--no_opsw_gw", "--logfile", "C:\buildlog\HP_SA\HP_SA_install.log", "--loglevel", "info" -Wait
-                        checkfile
-                    }
-                    else{
-                        writelog "32-bit architecture found" $log
-                        Start-Process "$ScriptPath\opsware-agent-60.0.70957.3-win32-5.2.exe" -ArgumentList "-f", "-s", "--withmsi", "--no_opsw_gw", "--logfile", "C:\buildlog\HP_SA\HP_SA_install.log", "--loglevel", "info" -Wait
-                        checkfile
-                    }
-            }
-         }
-        }
-      }
-     Catch{
-
-     writelog "Error occurred in installation of agent" $log
-     $Error[0]
-     Exit -1
-     } 
-
+         writelog "Error occurred in installation of agent" $log
+        $Error[0]
+        Exit -1
+     }
 }
 
 function checkfile(){
       
       Try {
             #We check the log file for any errors at install time and if so then stop the install
-            $loggingfile = Get-item C:\buildlog\HP_SA\*.log
+            $loggingfile = Get-item $LogFilePath\HP_SA\*.log
             $checkfile = $loggingfile | ? { ($_ | Select-String "No gateway address was specified")}
-               if($checkfile -eq $null){
+               if($null -eq $checkfile){
                      writelog "Installation succeeded" $log
                }
                  else{
                      writelog "***ERROR: Installation failed - the gateway entered is not contactable***" $log
-                     writelog "***ERROR: Please check error logs for the failure for HP SA - found in C:\buildlog\HP_SA***" $log
+                     writelog "***ERROR: Please check error logs for the failure for HP SA - found in $LogFilePath\HP_SA***" $log
                      throw "Installation failed - the gateway entered is not contactable"
               }
         }
       Catch {
-
-      $Error[0]
-      Exit -1
-
+        $Error[0]
+        Exit -1
       }
 }
 
-
-If ($SA_Server1 -ne "") {
-
-    writelog "IP/FQDN has been entered - proceeding with Live Installation of HP SA" $log
-    #checkIP $SA_Server1
+    writelog "Proceeding with Installation of HP SA" $log
+    checkIP $SA_Server1
     checkDiskSpace
-    #portFree
+    portFree
     OSandInstallIP $SA_Server1
-}
-else {
-
-    writelog "No IP entered so proceeding with Dormant Installation of HP SA" $log
-    checkDiskSpace
-    #portFree
-    OSandInstallDormant
-}
 
 writelog "$ScriptName Script ended" $log
 writelog "==============================" $log
